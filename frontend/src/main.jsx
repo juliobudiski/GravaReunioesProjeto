a8806486c6426689e42c5011cefacf76434c38c8
juliobudiski <juliobudiskiherculani@gmail.com>
2026-08-07 17:23:28 -0300

    fix: Removendo plugin PWA que travava o Build do Render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext.jsx'
-import { registerSW } from 'virtual:pwa-register'
-registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
